395529a5d6be5fd6c179477f53122e0c1636cda0
Mateusz Kupiniak <mkupiniak@gmail.com>
2019-09-14 14:01:30 +0200

    add prod build

---
+++ build/precache-manifest.ceac480d0283136a3dcfb551c7ae5bf0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9032b9a58abced1a42584121aa72e6bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fdea2fb99cf62eb7430e",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "39e6fae6eca3a5f9d80e",
+    "url": "/static/js/2.93f22626.chunk.js"
+  },
+  {
+    "revision": "fdea2fb99cf62eb7430e",
+    "url": "/static/js/main.ff166725.chunk.js"
+  },
+  {
+    "revision": "0ece2ea7f049e575314f",
+    "url": "/static/js/runtime~main.420e11b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
